74b0f29204d6dcad55dc2073ce7d9b98c3b31853
Eldiardz <eldiardzumabekov@gmail.com>
2025-01-27 15:00:47 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+document.querySelector(".true1").onclick = function(){
+    alert("Правильно! Молодец!")
+    document.querySelector(".questionbox1").style.display = "none"
+    document.querySelector(".questionbox2").style.display = "flex"
+}
+document.querySelector(".false1-1").onclick = function(){
+    alert("Неправильно!")
+}
+
+document.querySelector(".false1-2").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".true2").onclick = function(){
+    alert("Правильно! Молодец!")
+    document.querySelector(".questionbox2").style.display = "none"
+    document.querySelector(".questionbox3").style.display = "flex"
+}
+document.querySelector(".false2-1").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".false2-2").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".true3").onclick = function(){
+    alert("Правильно! Молодец!")
+    document.querySelector(".questionbox3").style.display = "none"
+    document.querySelector(".questionbox4").style.display = "flex"
+}
+document.querySelector(".false3-1").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".false3-2").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".true4").onclick = function(){
+    alert("Правильно! Молодец!")
+    document.querySelector(".questionbox4").style.display = "none"
+    document.querySelector(".questionbox5").style.display = "flex"
+}
+document.querySelector(".false4-1").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".false4-2").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".true5").onclick = function(){
+    alert("Правильно! Молодец!")
+    document.querySelector(".questionbox5").style.display = "none"
+    document.querySelector(".questionbox6").style.display = "flex"
+}
+document.querySelector(".false5-1").onclick = function(){
+    alert("Неправильно!")
+}
+document.querySelector(".false5-2").onclick = function(){
+    alert("Неправильно!")
+}
